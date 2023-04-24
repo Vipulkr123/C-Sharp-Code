80301fc959b8d205642385af3a8d721881176478
Vipul1432 <vipul.v@simformsolutions.com>
2023-04-24 11:23:22 +0530

    Add: tuples

--- ArrayDemo/ArrayDemo/Program.cs
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -4,20 +4,20 @@
     {
         static void Main(string[] args)
         {
-            // Single Dimension Array
+            //   Single Dimension Array
             int[] arr = new int[5] { 1, 2, 3, 4, 5 };
 
             for (int i = 0; i < arr.Length; i++)
             {
                 Console.Write(arr[i] + " ");
-            }
+            } 
             Console.WriteLine();
 
 
             Console.WriteLine("------------- Multi-Dimension Array --------------");
 
 
-            // Two Dimension Array 
+            // Two Dimension Array  
             int[,] arr2 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
 
             for (int i = 0; i < arr2.GetLength(0); i++)
@@ -81,15 +81,18 @@
 
             int[] firstArray = new int[5] { 1, 2, 3, 4, 5 };
             int[] secondArray = new int[3] { 6, 7, 9 };
-            secondArray.CopyTo(firstArray, 2);
+            secondArray.CopyTo(firstArray, 1);
             foreach (var item in firstArray)
             {
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+            Console.WriteLine("------------- Copy Array --------------");
             // Array Copy Method
-            Array.Copy(secondArray, firstArray, 3);
-            foreach (var item in firstArray)
+            int[] array1 = new int[5] { 1, 2, 3, 4, 5 };
+            int[] array2 = new int[3] { 6, 7, 8 };
+            Array.Copy(array2, array1, 0);
+            foreach (var item in array1)
             {
                 Console.Write(item + " ");
             }
@@ -98,8 +101,9 @@
             Console.WriteLine("------------- Array Methods --------------");
 
             //  arr =  1, 2, 3, 4, 5
-            Array.Reverse(arr);
-            foreach (var item in arr)
+            int[] newarr = new int[5] { 1, 2, 3, 4, -5 };
+            Array.Reverse(newarr);
+            foreach (var item in newarr)
             {
                 Console.Write(item + " ");
             }
---
+++ TuplesDemo/TuplesDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TuplesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuple<int,string> obj = new Tuple<int,string>(123, "Vipul");
+            Console.WriteLine(obj.Item1);
+            Console.WriteLine(obj.Item2);
+            
+            var tupleObj = Tuple.Create(123,"Mr.Vipul");
+            Console.WriteLine(tupleObj.Item1);
+            Console.WriteLine(tupleObj.Item2);
+
+            //! Value Tuple
+            var valueObj = (123, "Vipul Kumar");
+            Console.WriteLine(valueObj.Item1);
+            Console.WriteLine(valueObj.Item2);
+            valueObj.Item1 = 153;
+            Console.WriteLine(valueObj.Item1);
+        }
+    }
+}
