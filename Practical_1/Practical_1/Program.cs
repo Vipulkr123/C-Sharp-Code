1d6dfd4aa51f1fb8fb08c7c735ec8c78cd442a33
Vipul1432 <vipul.v@simformsolutions.com>
2023-04-27 18:01:44 +0530

    Practical 1

---
+++ Practical_1/Practical_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Practical_1
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string s = Console.ReadKey();
+            if (ConsoleKey.D0 >= Convert.ToInt32(s) )
+            {
+                Console.WriteLine("exit");
+            }
+
+        }
+    }
+}
